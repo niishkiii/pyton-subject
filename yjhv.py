e3c8913ecc97bccfef963e4a7910f1b19d24920f
niishkiii <baratellymax@gmail.com>
2023-10-21 14:17:42 +0300

    сделал програму

--- yjhv.py
+++ yjhv.py
@@ -1,1 +1,14 @@
-print("hi")
+
+while True:
+ try:
+    a = input("Введите ваш возраст: ")
+    if int(a) < 18:
+        print("Доступ запрещен.")
+        break
+    elif int(a) >= 18:
+        print("Доступ разрешен.")
+        break
+    else:
+        raise ValueError
+ except ValueError:
+    continue
